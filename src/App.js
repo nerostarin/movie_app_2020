9d58bb9ca244876a5a252f368e17fdda43b4add5
LeeTaeJung <nerostarin@naver.com>
2020-09-11 12:39:07 +0900

    Edit file 0911

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 function App() {
-  return <div className="App"/>;
+  //return <div className="App"/>;
+  return (<div><h1>Hello!!! mr my yesterday </h1></div>);
 }
 
 export default App;
