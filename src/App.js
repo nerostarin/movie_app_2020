995f04e49e532db9b76a67d44f0a56ff21c29928
LeeTaeJung <nerostarin@naver.com>
2020-09-25 12:53:30 +0900

    Edit 0925

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
 import React from 'react';
+import img01 from './images/bg1.jpg';
+import img02 from './images/bg2.jpg';
+import img03 from './images/bg3.jpg';
+import img04 from './images/bg4.jpg';
 
-function Food({fav})
+
+function Food({name, picture, message0})
 {
    return (
-  <h3>I love Food{fav}</h3>
+    <div>
+  <h3>I love Food{name}</h3>
+  <img src={picture} alt={message0}/>
+  </div>
   );
 }
+
+const foodILike = [
+  {
+    id: 1,
+    name:'풍경',
+    image: img01,
+    message0:"풍경사진"
+  }
+  ,
+  {
+    id: 2,
+    name:'풍경1',
+    image:img02,
+    message0:"풍경사진"
+
+  },
+  { id: 3,
+    name:'풍경2',
+    image:img03,
+    message0:"풍경사진"
+
+  },
+  {
+    id: 4,
+    name:'풍경3',
+    image:img04,
+    message0:"풍경사진"
+
+  },
+  {
+    id: 5,
+    name:'풍경4',
+    image:img04,
+    message0:"풍경사진"
+
+  }
+];
 
 function App() {
   //return <div className="App"/>;
   return (<div>
-    <h1>Hello!!! mr my yesterday </h1>
-    <Food fav="김치"/>  
-    <Food fav="짜장면"/>  
-    <Food fav="짬뽕"/>  
-    <Food fav="만두"/>  
-    <Food fav="피자"/>  
-
-  
+    {foodILike.map(dish => (<Food key={dish.id} name={dish.name} picture = {dish.image} message0m={dish.alt}/>))}
   </div>);
 }
 
