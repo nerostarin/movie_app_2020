5b80a11681dcfbb3e8eab19d187132dfe8c16aa3
LeeTaeJung <nerostarin@naver.com>
2020-11-20 12:39:03 +0900

    Edit 1120

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -21,11 +21,11 @@ class Detail extends React.Component
        return(
            <div className="movie">
            <div className="movie-data">
-           <h3>{location.state.title}</h3>
+           <h3 className="movie-title">{location.state.title}</h3>
            <img src={location.state.poster} className="image"/>
-           <h4>{location.state.year}</h4>
-           <h3>{location.state.genres}</h3>
-           <h3>{location.state.summary}</h3>
+           <h4 className="movie-year">{location.state.year}</h4>
+           <h3 className="movie-genre">{location.state.genres}</h3>
+           <h3 className="movie-summary">{location.state.summary}</h3>
            </div>
            </div>
         );
