5925117baa8b19c627b3912b404298b97b0489f8
LeeTaeJung <nerostarin@naver.com>
2020-11-20 12:46:53 +0900

    Updates

---
+++ precache-manifest.42bbe57b0bc46619dd24a3328548748d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "066d85a90597c5008cca56c8371b41fa",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "b229805b9112a521687f",
+    "url": "/movie_app_2020/static/css/main.7fd3da15.chunk.css"
+  },
+  {
+    "revision": "ca1290ec655463a20b0e",
+    "url": "/movie_app_2020/static/js/2.ad030fd9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.ad030fd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b229805b9112a521687f",
+    "url": "/movie_app_2020/static/js/main.27e4dff2.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
